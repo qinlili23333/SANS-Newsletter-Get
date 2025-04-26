2fb37db82b32868f2ab673c58c95e8e9058425c0
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-04-26 23:18:32 +1000

    [js] Parse data

--- Ouch/EmbedJS/ExtractJSON.js
+++ Ouch/EmbedJS/ExtractJSON.js
@@ -1,2 +1,46 @@
-const bridge = chrome.webview.hostObjects.bridge;
-bridge.Log("Extract script loaded. Initialize bridge communication...");
+(async () => {
+    const bridge = chrome.webview.hostObjects.bridge;
+    await bridge.Log("Extract script loaded. Bridge communication established.");
+    const sleep = ms => new Promise(r => setTimeout(r, ms))
+    await bridge.Log("Waiting for list fully loaded...");
+    // Wait until list loaded
+    while (document.getElementsByClassName("article-teaser-large").length == 0) {
+        await sleep(100);
+    }
+    await bridge.Log("Finding webpack function...");
+    let jsonpList = [];
+    for (let js in webpackJsonp) {
+        for (let num in webpackJsonp[js][1]) {
+            const jsonpString = webpackJsonp[js][1][num].toString();
+            if (jsonpString.indexOf("exports=JSON.parse('") > 0 && jsonpString.length > 1000000) {
+                jsonpList.push(webpackJsonp[js][1][num])
+            }
+        }
+    }
+    console.log(jsonpList);
+    await bridge.Log("Converting to Object...");
+    let jsonObjList = [];
+    for (let jsonp of jsonpList) {
+        let tempObj = {};
+        jsonp(tempObj);
+        jsonObjList.push(tempObj.exports);
+    }
+    await bridge.Log("Parsing...");
+    let jsonDict = {};
+    const getName = obj => {
+        let title = obj[0].metainfo.browser_title;
+        if (title.indexOf("OUCH") > 0) {
+            return "OUCH";
+        }
+        if (title.indexOf("@RISK") > 0) {
+            return "RISK";
+        }
+        if (title.indexOf("NewsBites") > 0) {
+            return "NB";
+        }
+    }
+    for (let obj of jsonObjList) {
+        jsonDict[getName(obj)] = JSON.stringify(obj);
+    }
+    await bridge.Log("Writing to disk...");
+})();
